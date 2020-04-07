3b049cd0aae35bbb4a5d7ffa16c008d25afffb27
praveen-tissera <praveen.tissera@gmail.com>
2020-04-07 08:57:56 +0530

    constant

---
+++ constant.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>PHP contstant</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    <?php 
+    //define a contant
+    define('HOSTNAME','codewrapper.com',true);
+
+    echo HOSTNAME;
+    echo "<hr>";
+    echo hostname;
+
+
+    
+    
+    
+    ?>
+    </body>
+</html>
